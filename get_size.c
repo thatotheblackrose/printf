cb64b896b94f71ef180f1311578d8e4177b721e2
Eby88 <enwaneri88@gmail.com>
2023-07-22 09:28:05 -0700

    Eby88

---
+++ get_size.c
@@ -0,0 +1,23 @@
+#include &quot;main.h&quot;
+/**
+* get_size - Calculates the size to cast the argument
+* @format: Formatted string in which to print the arguments
+* @i: List of arguments to be printed.
+*
+* Return: Precision.
+*/
+int get_size(const char *format, int *i)
+{
+int curr_i = *i + 1;
+int size = 0;
+if (format[curr_i] == &#39;l&#39;)
+size = S_LONG;
+else if (format[curr_i] == &#39;h&#39;)
+size = S_SHORT;
+if (size == 0)
+*i = curr_i - 1;
+else
+*i = curr_i;
+
+return (size);
+}
